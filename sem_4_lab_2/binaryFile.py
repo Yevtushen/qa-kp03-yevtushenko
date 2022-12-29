11936e16c3c9efdc9616c431181aac7bf34df2ab
Євтушенко Вікторія [КП-03] <victoria.evtushenko.0@gmail.com>
2022-12-29 19:55:50 +0200

    entities implemented

--- sem_4_lab_2/binaryFile.py
+++ sem_4_lab_2/binaryFile.py
@@ -1,13 +1,34 @@
 class BinaryFile:
 
     def __init__(self, name: str, directory, contents: str):
-        pass
+        if directory.count < directory.max_size:
+            self.directory = directory
+            self.directory.count = 1
+            self.name = name
+            self.contents = contents
+            self.directory.list.append(self)
+        else:
+            print("This directory is already full")
+            return
 
     def delete(self):
-        pass
+        print("Deleting " + self.name)
+        index = self.directory.list.index(self)
+        self.directory.list.pop(index)
+        del self
+        print("Deleted successfully")
 
     def move_binary_file(self, location):
-        pass
+        if location.count < location.max_size:
+            self.directory.count -= 1
+            index = self.directory.list.index(self)
+            self.directory.list.pop(index)
+            self.directory = location
+            self.directory.list.append(self)
+            self.directory.count += 1
+        else:
+            print("Directory is already full")
+            return
 
     def read_file(self):
-        pass
+        print(self.contents)
--- sem_4_lab_2/directory.py
+++ sem_4_lab_2/directory.py
@@ -1,13 +1,47 @@
 class Directory:
 
     def __init__(self, name: str, parent_directory, max_size: int):
-        pass
+        if (parent_directory is not None) and (parent_directory.count < parent_directory.max_size):
+            parent_directory.count += 1
+            parent_directory.list.append(self)
+            self.name = name
+            self.parent_directory = parent_directory
+            self.max_size = max_size
+            self.count = 0
+            self.list = []
+        elif parent_directory is None:
+            self.name = name
+            self.parent_directory = parent_directory
+            self.max_size = max_size
+            self.count = 0
+            self.list = []
+        elif parent_directory.count >= parent_directory.max_size:
+            print("This directory is already full")
+            return
 
     def delete(self):
-        pass
+        print("Deleting " + self.name)
+        index = self.parent_directory.list.index(self)
+        self.parent_directory.list.pop(index)
+        del self
+        print("Deleted successfully")
 
     def list_elements(self):
-        pass
+        resulting_list = 'In ' + self.name + ': '
+        for element in self.list:
+            resulting_list += element.name + '; '
+        print(resulting_list)
 
     def move_directory(self, location):
-        pass
+        if self.parent_directory is None:
+            print("Impossible to move root directory")
+        if location.count < location.max_size + self.count + 1:
+            self.parent_directory.count -= self.count + 1
+            index = self.parent_directory.list.index(self)
+            self.parent_directory.list.pop(index)
+            self.parent_directory = location
+            self.parent_directory.list.append(self)
+            self.parent_directory.count += 1
+        else:
+            print("Directory is already full")
+            return
