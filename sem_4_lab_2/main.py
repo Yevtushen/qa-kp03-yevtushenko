38ebacddd0561c540a7f2a8a10bfe1cd78c64306
Євтушенко Вікторія [КП-03] <victoria.evtushenko.0@gmail.com>
2023-01-03 23:02:37 +0200

    api implementation

--- sem_4_lab_2/main.py
+++ sem_4_lab_2/main.py
@@ -1,48 +1,259 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+from directory import Directory
+from binaryFile import BinaryFile
+from logTextFile import LogTextFile
+from bufferFile import BufferFile
 
 app = Flask(__name__)
+
+
+root = Directory('root', None, 100)
+trash = []
+
 
 @app.route('/directory', methods=['POST', 'GET', 'PATCH', 'DELETE'])
 def directory():
    if request.method == 'POST':
-      return 400
+      if any(element.name == request.args.get('name') for element in root.list) or request.args.get('name')=='root':
+         return jsonify({'message': 'Such directory already exists.'}), 400
+      dir = Directory(request.args.get('name'), root, int(request.args.get('max_size')))
+      return jsonify({
+         'message': 'Created successfully.',
+         'directory': {
+            'parent_directory': dir.parent_directory,
+            'name': dir.name,
+            'max_size': dir.max_size
+      }
+      }), 201
+
    elif request.method == 'GET':
-      return 400
+      if any(dir.name == request.args.get('name') for dir in root.list) or request.args.get('name') == 'root':
+         if request.args.get('name') == 'root':
+            dir = root
+         else:
+            dir = next(element for element in root.list if element.name == request.args.get('name'))
+         return jsonify({
+            'message': 'Created successfully.',
+            'directory': {
+               'parent_directory': dir.parent_directory,
+               'name': dir.name,
+               'max_size': dir.max_size
+            }
+         }), 200
+      return jsonify({'message': 'Directory does not exist.',}), 400
+
    elif request.method == 'PATCH':
-      return 400
+      if any(dir.name == request.args.get('name') for dir in root.list):
+         dir = next(element for element in root.list if element.name == request.args.get('name'))
+         dir.move_directory(root)
+         return jsonify({
+            'message': 'Moved successfully.',
+            'directory': {
+               'parent_directory': dir.parent_directory,
+               'name': dir.name,
+               'max_size': dir.max_size
+            }
+         }), 200
+      return jsonify({'message': 'Directory does not exist.',}), 400
+
    else:
-      return 400
+      if request.args.get('name') not in trash and any(dir.name == request.args.get('name') for dir in root.list):
+         dir = next(x for x in root.list if x.name == request.args.get('name'))
+         dir.delete()
+         trash.append(request.args.get('name'))
+         return jsonify({
+            'message': 'Deleted successfully.',
+         }), 200
+      return jsonify({'message': 'Deleting failed.'}), 400
+
 
 @app.route('/binaryfile', methods=['POST', 'GET', 'PATCH', 'DELETE'])
 def binaryfile():
+   file = BinaryFile(request.args.get('name'), root, request.args.get('info'))
    if request.method == 'POST':
-      return 400
+      if any(element.name == request.args.get('name') for element in root.list):
+         return jsonify({
+            'message': 'Such file already exists.',
+         }), 400
+      file = BinaryFile(request.args.get('name'), root, request.args.get('info'))
+      return jsonify({
+         'message': 'Created successfully.',
+         'file': {
+            'directory': file.directory.name,
+            'name': file.name,
+            'contents': file.contents
+         }
+      }), 201
+
    elif request.method == 'GET':
-      return 400
+      if any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         return jsonify({
+            'message': 'Read successfully.',
+            'file': {
+               'directory': file.directory.name,
+               'name': file.name,
+               'contents': file.contents
+            }
+         }), 200
+      return jsonify({'message': "Such file does not exist."}), 400
+
    elif request.method == 'PATCH':
-      return 400
+      if any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         file.move(root)
+         return jsonify({
+            'message': 'Moved successfully.',
+            'file': {
+               'directory': file.directory.name,
+               'name': file.name,
+               'content': file.content
+            }
+         }), 200
+      return jsonify({'message': "Such file does not exist."}), 400
+
    else:
-      return 400
+      if request.args.get('name') not in trash and any(file.name == request.args.get('name') for file in root.list):
+         file = next(x for x in root.list if x.name == request.args.get('name'))
+         file.delete()
+         trash.append(request.args.get('name'))
+         return jsonify({'message': "Deleted successfully."}), 200
+      return jsonify({"message": "Deleting failed."}), 400
+
 
 @app.route('/logtextfile', methods=['POST', 'GET', 'PATCH', 'DELETE'])
 def logtextfile():
    if request.method == 'POST':
-      return 400
+      if any(element.name == request.args.get('name') for element in root.list):
+         return jsonify({
+            'message': "Such file already exists.",
+         }), 400
+      file = LogTextFile(request.args.get('name'), root, request.args.get('contents'))
+      return jsonify({
+         'message': "Created successfully.",
+         'file': {
+            'directory': file.directory.name,
+            'name': file.name,
+            'contents': file.contents
+         }
+      }), 201
    elif request.method == 'GET':
-      return 400
+      if any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         return jsonify({
+            'message': "Created successfully.",
+            'file': {
+               'directory': file.directory.name,
+               'name': file.name,
+               'contents': file.contents
+            }
+         }), 200
+      return jsonify({'message': 'Such file does not exist.'}), 400
    elif request.method == 'PATCH':
-      return 400
+      if any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         if request.args.get('parent'):
+            file.move_log_file(root)
+            return jsonify({
+               'message': "Moved successfully.",
+               'file': {
+                  'directory': file.directory.name,
+                  'name': file.name,
+                  'contents': file.contents
+            }
+            }), 200
+         elif request.args.get('append'):
+            file.append_line(request.args.get('append'))
+            return jsonify({
+               'message': "Line appended successfully.",
+               'file': {
+                  'directory': file.directory.name,
+                  'name': file.name,
+                  'contents': file.contents
+            }
+            }), 201
+         return jsonify({'message': 'Wrong request.'}), 400
+      return jsonify({'message': 'File does not exist.'}), 400
    else:
-      return 400
+      if request.args.get('name') not in trash and any(file.name == request.args.get('name') for file in root.list):
+         file = next(x for x in root.list if x.name == request.args.get('name'))
+         file.delete()
+         trash.append(request.args.get('name'))
+         return jsonify({'message': "Deleted successfully."}), 200
+      return jsonify({'message': 'Deleting failed.'}), 400
+
+
 
 @app.route('/bufferfile', methods=['POST', 'GET', 'PATCH', 'DELETE'])
 def bufferfile():
    if request.method == 'POST':
-      return 400
+      if any(element.name == request.args.get('name') for element in root.list):
+         return jsonify({'message': 'Such file already exists.'}), 400
+      file = BufferFile(request.args.get('name'), root, int(request.args.get('max_size')))
+      return jsonify({
+         'message': 'Created successfully.',
+         'file': {
+            'directory': file.directory,
+            'name': file.name,
+            'max_size': file.max_size,
+         }
+      }), 201
    elif request.method == 'GET':
-      return 400
+      if any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         return jsonify({
+            'message': 'Read successfully.',
+            'file': {
+               'directory': file.directory,
+               'name': file.name,
+               'max_size': file.max_size,
+            }
+         }), 200
+      return jsonify({'message': 'Such file does not exist.'}), 400
+   elif request.method == 'PATCH':
+      if any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         if request.args.get('directory'):
+            file.move(root)
+            return jsonify({
+               'message': 'Moved successfully.',
+               'file': {
+                  'directory': file.directory,
+                  'name': file.name,
+                  'max_size': file.max_size,
+            }
+            }), 200
+         if request.args.get('push'):
+            file.push_element(request.args.get('push'))
+            return jsonify({
+               'message': 'Element pushed successfully.',
+               'file': {
+                  'directory': file.directory,
+                  'name': file.name,
+                  'max_size': file.max_size,
+            }
+            }), 201
+         if request.args.get('consume'):
+            if len(file.contents) > 0:
+               file.consume_element(request.args.get('consume'))
+            return jsonify({
+               'message': 'Element consumed successfully.',
+               'file': {
+                  'directory': file.directory,
+                  'name': file.name,
+                  'max_size': file.max_size,
+            }
+            }), 200
+         return jsonify({'message': 'Wrong request.'}), 400
+      return jsonify({'message': 'Such file does not exist.'}), 400
    else:
-      return 400
+      if request.args.get('name') not in trash and any(file.name == request.args.get('name') for file in root.list):
+         file = next(element for element in root.list if element.name == request.args.get('name'))
+         file.delete()
+         trash.append(request.args.get('name'))
+         return jsonify({'message': 'Deleted successfully.'}), 200
+      return jsonify({'message': 'Deleting failed'}), 400
 
-if __name__ = '__main__':
+
+if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
